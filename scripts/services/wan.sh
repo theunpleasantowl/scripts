d47c05210a29202d4c33461c6e8c20589eedd55c
Hibiki <theunpleasantowl@gmail.com>
2019-03-10 16:08:46 -0400

    add she-bang

--- scripts/services/wan.sh
+++ scripts/services/wan.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 cd ~/Services
 if [ ! -f $(pwd)/wan.txt ]; then
     echo "" > wan.txt  ; sed -i "s/^.*/\n\n\n\n\n/" $(pwd)/wan.txt
@@ -8,5 +9,5 @@ oldwan="$(sed -n 5p wan.txt)"
 
 if [ "$wan" != "$oldwan" ]; then
 	sed -i "5s/^.*/$wan/" $(pwd)/wan.txt
-	msmtp -a default theunpleasantowl@gmail.com < wan.txt 
+	msmtp -a default theunpleasantowl@gmail.com < wan.txt
 fi
